7b7cfcda1ecc91c995b6486c97d0cde10d015d72
hamza732387 <hearthard40@gmail.com>
2020-03-18 21:21:55 +0300

    ff

--- Front-End/signup/Sign up.js
+++ Front-End/signup/Sign up.js
@@ -152,7 +152,7 @@ function register(){
           Email_SignUp_P.innerHTML = "";
           localStorage.setItem("token",data.token)
           localStorage.setItem("id" , data.id)    
-          window.location.replace("../profile/profile.html")
+          window.location.replace("../../Front-End/profile/profile.html")
         }
     })
   }
