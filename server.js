4cea0470725583cce75fc10b1fa45ce5b5a4935c
hamza732387 <hearthard40@gmail.com>
2020-03-18 21:12:47 +0300

    jj

--- server.js
+++ server.js
@@ -17,6 +17,24 @@ app.use(trainee)
 app.get("/" , (req , res)=>{
     res.sendFile(__dirname + '/Front-End/home/index.html')
 })
+app.get("/SignUp" , (req , res)=>{
+    res.sendFile(__dirname + '/Front-End/signup/Sign Up.html')
+})
+app.get("/profile" , (req , res)=>{
+    res.sendFile(__dirname + '/Front-End/profile/profile.html')
+})
+app.get("/EditePassword" , (req , res)=>{
+    res.sendFile(__dirname + '/Front-End/changPassword/EditePassword.html')
+})
+app.get("/myphoto" , (req , res)=>{
+    res.sendFile(__dirname + '/Front-End/my photo/myphoto.html')
+})
+app.get("/Log in" , (req , res)=>{
+    res.sendFile(__dirname + '/Front-End/login/Log in.html')
+})
+app.get("/profile" , (req , res)=>{
+    res.sendFile(__dirname + '/Front-End/innerProfile/profile.html')
+})
 
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`);
